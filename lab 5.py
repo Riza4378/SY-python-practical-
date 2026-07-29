ef1cef98b73b32e9c90606f7078c7ace18881c43
Riza4378 <sajjadshaikh8790@gmail.com>
2026-07-29 13:02:44 +0530

    Add files via upload

---
+++ lab 5.py
@@ -0,0 +1,9 @@
+Expenses=0.0
+while True:
+    value=float(input ("enter value :"))
+    if value==-1:
+        break
+    Expenses=Expenses+value
+    print("Total Expenses=" ,Expenses)
+    
+     
