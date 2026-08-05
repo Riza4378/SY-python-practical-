1334cbfaf91b111c60ead77137d0170a1ba4f104
Riza4378 <sajjadshaikh8790@gmail.com>
2026-08-05 13:55:48 +0530

    Add files via upload

--- lab 5.py
+++ lab 5.py
@@ -1,9 +1,37 @@
-Expenses=0.0
+print("============ Monthly Expense Tracker =============")
+food=0
+travel=0
+shopping=0
+total=0
+others=0
+
 while True:
-    value=float(input ("enter value :"))
+    value=float(input("Enter your amount :"))
     if value==-1:
         break
-    Expenses=Expenses+value
-    print("Total Expenses=" ,Expenses)
     
-     
+    category=str(input("Enter a category (food/travel/shopping/others):")).lower()
+    
+    if category=="food":
+        food= food +  value
+    elif category=="travel":
+        travel= travel + value
+    elif category=="shopping":
+        shopping= shopping + value 
+    elif category == "others":
+        others = others + value           
+   
+        
+        
+total = food + travel + shopping + others           
+        
+        
+print("\n========Expenses summary=======")
+print("food:",food) 
+print("travel:",travel)
+print("shopping:",shopping)
+print("others:",others)
+print("total  :", total)       
+
+
+
