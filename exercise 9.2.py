c5475ab35581ebc3f8b8dc4e2116435ad58c08ab
Riza4378 <sajjadshaikh8790@gmail.com>
2026-09-01 13:39:35 +0530

    Add files via upload

---
+++ exercise 9.2.py
@@ -0,0 +1,10 @@
+grades = [80, 85, 90, 95, 100]
+
+print("Original grades:", grades)
+
+index = int(input("Enter index position: "))
+new_grade = int(input("Enter new grade: "))
+
+grades[index] = new_grade
+
+print("Corrected grades:", grades)
